9d44a5ba893883ffcf614eddc6d53330dd8d5e98
carlosmsal22 <CARLOSMSAL22@GMAIL.COM>
2025-04-18 08:28:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-import os 
-import cv2 from keras.applications.resnet50 
-import ResNet50 from keras.preprocessing 
-import image from keras.applications.resnet50 
-import preprocess_input, decode_predictions
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image
