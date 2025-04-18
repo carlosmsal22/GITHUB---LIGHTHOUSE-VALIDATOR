3a9bc08fcae531c87d703191a518a2bb9d7b129d
carlosmsal22 <CARLOSMSAL22@GMAIL.COM>
2025-04-18 08:39:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+# main.py
+
 import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from fastapi import FastAPI
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from keras.preprocessing import image
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Lighthouse Validator is live and ready 🚀"}
