2e5ed358a4860b3791ec02bb1e3f5287d8be38b1
carlosmsal22 <CARLOSMSAL22@GMAIL.COM>
2025-04-18 09:51:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from fastapi import FastAPI
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-from keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
 
 app = FastAPI()
 
